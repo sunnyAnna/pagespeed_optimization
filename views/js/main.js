f73eba2494a39ebd1cdafc23f9ac5ba55c3965d2
Anna <annakrygier@hotmail.com>
2016-05-17 18:13:33 -0700

    changed the initial pizza menu images size to small

--- views/js/main.js
+++ views/js/main.js
@@ -371,9 +371,6 @@ function PizzaElementGenerator(i) {
 };
 
 
-//var z = document.querySelectorAll(".randomPizzaContainer .img-responsive");
-//var originalWidth = z[0].offsetWidth;
-
 // resizePizzas(size) is called when the slider in the "Our Pizzas" section of the website moves.
 var resizePizzas = function (size) {
     window.performance.mark("mark_start_resize"); // User Timing API function
